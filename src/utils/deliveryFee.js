3fe663b0141fa14bfc024f0c107b3ad6a6f42dec
benson morris <bensonseverino@hotmail.com>
2026-08-08 09:28:23 +0300

    delivery fees

--- src/utils/deliveryFee.js
+++ src/utils/deliveryFee.js
@@ -1,15 +1,15 @@
 // utils/deliveryFee.js
 
 export const DELIVERY_ZONES = [
-  { name: "Nairobi CBD", fee: 200 },
-  { name: "Westlands", fee: 150 },
-  { name: "Kilimani", fee: 150 },
+  { name: "Nairobi CBD", fee: 100 },
+  { name: "Westlands", fee: 250 },
+  { name: "Kilimani", fee: 300 },
   { name: "Thika", fee: 300 },
-  { name: "Mombasa", fee: 400 },
-  { name: "Other", fee: 500 },
+  { name: "Mombasa", fee: 300 },
+  { name: "Other", fee: 400 },
 ];
 
 export const getDeliveryFee = (zone) => {
   const selected = DELIVERY_ZONES.find((z) => z.name === zone);
-  return selected ? selected.fee : 500;
+  return selected ? selected.fee : 400;
 };
